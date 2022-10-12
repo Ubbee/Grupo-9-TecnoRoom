b87bc72add0784ba26df53927be2c77f4bb7c150
ElioM10 <eliomari48@gmail.com>
2022-10-11 21:02:41 -0300

    Creacion

---
+++ TecnoRoom/src/test/java/com/example/tecnoroom/TecnoRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tecnoroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TecnoRoomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
