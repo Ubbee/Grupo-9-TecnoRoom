82b5067d32fb004658b8ce659e28ca47f13ad62b
ElioM10 <eliomari48@gmail.com>
2022-10-11 21:52:32 -0300

    Commit prueba

--- TecnoRoom/src/main/java/com/example/tecnoroom/TecnoRoomApplication.java
+++ TecnoRoom/src/main/java/com/example/tecnoroom/TecnoRoomApplication.java
@@ -9,5 +9,5 @@ public class TecnoRoomApplication {
     public static void main(String[] args) {
         SpringApplication.run(TecnoRoomApplication.class, args);
     }
-
+    dsjfkaskdjasjdljslalñdñasld
 }
