8604d0cd062124d2f0e72ac1ede749f36ee27447
ElioM10 <eliomari48@gmail.com>
2022-10-16 23:48:30 -0300

    correcion DetalleRepository

---
+++ TecnoRoom/src/main/java/com/example/tecnoroom/controllers/DetalleController.java
@@ -0,0 +1,14 @@
+package com.example.tecnoroom.controllers;
+
+import com.example.tecnoroom.entities.DetalleOrden;
+import com.example.tecnoroom.services.DetalleServiceImpl;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("tecnoRoom/v1/detalle")
+public class DetalleController extends BaseControllerImpl<DetalleOrden, DetalleServiceImpl>{
+
+}
---
+++ TecnoRoom/src/main/java/com/example/tecnoroom/repositories/DetalleRepository.java
@@ -0,0 +1,8 @@
+package com.example.tecnoroom.repositories;
+
+import com.example.tecnoroom.entities.DetalleOrden;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DetalleRepository extends BaseRepository<DetalleOrden,Long>{
+}
