7db1a4a6908261f61bbda2622523ad72c8140793
Santiago Curotto <santiago.curotto.98@gmail.com>
2022-10-22 00:25:51 -0300

    arreglo error admin

--- TecnoRoom/src/main/java/com/example/tecnoroom/services/administradorService.java
+++
@@ -1,7 +0,0 @@
-package com.example.tecnoroom.services;
-
-import com.example.tecnoroom.entities.Administrador;
-
-
-public interface administradorService extends BaseService<Administrador,Long>{
-}
