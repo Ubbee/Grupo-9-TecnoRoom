0747989d65b7f12271e904f2450892b3965679ff
Santiago Curotto <santiago.curotto.98@gmail.com>
2022-11-12 19:22:19 -0300

    errors fixed

--- TecnoRoom/src/main/java/com/example/tecnoroom/controllers/ProductoController.java
+++ TecnoRoom/src/main/java/com/example/tecnoroom/controllers/ProductoController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -43,4 +44,17 @@ public class ProductoController extends BaseControllerImpl<Producto, ProductoSer
             return "Error";
         }
     }
+
+    @GetMapping("/producto/{id}")
+    public String producto(Model model, @PathVariable("id") Long id){
+        try{
+            Producto productos = productoService.findById(id);
+            model.addAttribute("productos", productos);
+            return "indexProd";
+        }catch (Exception e){
+            model.addAttribute("Error",e.getMessage());
+            return "Error";
+        }
+    }
+
 }
